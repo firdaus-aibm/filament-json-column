01890c20851f349f253fc3713d9834132d21626d
Ahmad Firdaus <firdaus.samsuddin@aeoninsurance.com.my>
2025-06-24 16:11:17 +0800

    update to use local json editor

--- src/FilamentJsonColumnServiceProvider.php
+++ src/FilamentJsonColumnServiceProvider.php
@@ -28,10 +28,10 @@ class FilamentJsonColumnServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            Js::make('jsoneditor-js-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.0.2/jsoneditor.min.js'),
+	        Js::make('jsoneditor-js-cdn', __DIR__.'/../resources/js/jsoneditor.min.js'),
             Js::make('filament-json-column-js', __DIR__.'/../resources/js/filament-json-column.js'),
             Css::make('filament-json-column', __DIR__.'/../resources/css/filament-json-column.css'),
-            Css::make('jsoneditor-css-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.0.2/jsoneditor.min.css'),
+	        Css::make('jsoneditor-css-cdn', __DIR__.'/../resources/css/jsoneditor.min.css'),
         ], 'filament-json-column');
     }
 }
